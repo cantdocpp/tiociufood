45d96403d23f266d458f4532412d89712262a51f
patrick Patrick <cihuyman95@gmail.com>
2021-10-29 23:30:03 +0700

    implement add admin service

--- backend/services/auth/src/admin.js
+++ backend/services/auth/src/admin.js
@@ -1,12 +1,32 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+const bcrypt = require('bcrypt');
 const { response } = require('../../../utils/response')
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2'
+})
 
 module.exports = {
     add: async (event) => {
-        return response(200, {
-            status: 'ok',
-            message: 'api success'
-        })
+        const { email, password } = event
+        const hashedPassword = await bcrypt.hash(password, 10)
+        try {
+            await docClient.put({
+                TableName: 'adminTable',
+                Item: {
+                    "email": email,
+                    "password": hashedPassword
+                }
+            }).promise()
+            return response(200, {
+                message: 'Add admin success'
+            })
+        } catch(err) {
+            return response(500, {
+                message: 'Something wrong on the server'
+            })
+        }
     }
 }
