ca07fbfcb3f8987d51d08930fdbaf3d8a3632aaa
patrick Patrick <cihuyman95@gmail.com>
2021-10-30 03:53:47 +0700

    implement user add

--- backend/services/auth/src/admin.js
+++ backend/services/auth/src/admin.js
@@ -2,6 +2,7 @@
 
 const AWS = require('aws-sdk')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const docClient = new AWS.DynamoDB.DocumentClient({
     region: 'us-east-2'
@@ -12,7 +13,7 @@ module.exports = {
         const data = JSON.parse(event.body)
         const { email, password } = data
         const hashedPassword = await bcrypt.hash(password, 10)
-        
+
         try {
             await docClient.put({
                 TableName: 'adminTable',
@@ -41,7 +42,88 @@ module.exports = {
                     "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
                 },
                 body: JSON.stringify({
-                    message: err
+                    message: 'Something is wrong on the server'
+                })
+            }
+        }
+    },
+    login: async (event) => {
+        // for dev
+        // const { email, password } = event
+
+        // for staging
+        const data = JSON.parse(event.body)
+        const { email, password } = JSON.parse(event.body)
+
+        try {
+            const adminSearchResult = await docClient.query({
+                TableName: 'adminTable',
+                KeyConditionExpression: '#e = :email',
+                ExpressionAttributeNames: {
+                    '#e': 'email'
+                },
+                ExpressionAttributeValues: {
+                    ':email': email
+                }
+            }).promise()
+
+            console.log(adminSearchResult)
+            
+            if (!adminSearchResult.Items) {
+                return {
+                    statusCode: 401,
+                    headers: {
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                    },
+                    body: JSON.stringify({
+                        message: 'username or password is wrong'
+                    })
+                }
+            }
+
+            const adminPassword = adminSearchResult.Items[0].password
+            const isPasswordValid = await bcrypt.compare(password, adminPassword)
+            
+            if (isPasswordValid) {
+                const adminObj = adminSearchResult.Items[0]
+                const token = jwt.sign(adminObj, 'tiociufood')
+                
+                return {
+                    statusCode: 401,
+                    headers: {
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                    },
+                    body: JSON.stringify({
+                        access_token: token
+                    })
+                }
+            }
+
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'AUTH ERROR: Something is wrong on the server'
+                })
+            }
+        } catch(err) {
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'Something is wrong on the server'
                 })
             }
         }
