b37a2c3df0991cbced5aace21ab571ac0b8987d7
patrick Patrick <cihuyman95@gmail.com>
2021-11-01 01:47:37 +0700

    implement basic auth flow

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <div id="nav">
+  <div id="nav" v-if="!loggedIn">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
 
+<script>
+import { authComputed } from './utils/helper'
+
+export default {
+  computed: {
+    ...authComputed
+  }
+}
+</script>
+
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Sora', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: #000
 }
 </style>
--- frontend/admin/src/store/index.js
+++ frontend/admin/src/store/index.js
@@ -1,8 +1,33 @@
 import { createStore } from "vuex";
+import axios from 'axios';
 
 export default createStore({
   state: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_USER_DATA (state, userData) {
+      state.user = userData
+      localStorage.setItem("user", JSON.stringify(userData))
+      axios.defaults.headers.common["Authorization"] = `Bearer ${
+        userData.token
+      }`
+    }
+  },
+  actions: {
+    login ({ commit }, credentials) {
+      return axios
+        .post("https://u7aiz0a2l6.execute-api.us-east-2.amazonaws.com/dev/api/v1/auth/admin/login", credentials)
+        .then(({ data }) => {
+          commit("SET_USER_DATA", data);
+        })
+        .catch(err => {
+          console.log(err.response)
+        })
+    },
+  },
   modules: {},
+  getters: {
+    loggedIn (state) {
+      return !!state.user
+    }
+  }
 });
