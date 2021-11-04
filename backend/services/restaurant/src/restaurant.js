250d18902dcb7e422bdd389a3c18c9a9df12a0ab
patrick Patrick <cihuyman95@gmail.com>
2021-11-05 04:01:05 +0700

    implement get restaurants

--- backend/services/restaurant/src/restaurant.js
+++ backend/services/restaurant/src/restaurant.js
@@ -20,7 +20,7 @@ module.exports = {
       await docClient.put({
         TableName: 'tiociufoodTable',
         Item: {
-          pk: `RESTAURANT#${name}`,
+          identifier: `RESTAURANT#${name}`,
           sk: `RESTAURANT#${name}`,
           restaurantName: name,
           restaurantAddress: address,
@@ -39,7 +39,53 @@ module.exports = {
         })
       }
     } catch(err) {
-      console.log(err)
+      return {
+        statusCode: 500,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        body: JSON.stringify({
+            message: 'something is wrong on the server',
+            error: err
+        })
+      }
+    }
+  },
+  getRestaurant: async (event) => {
+    // for dev
+    // const { restaurantName } = event
+
+    // for staging
+    // const data = JSON.parse(event.body)
+    // const { restaurantName } = data
+
+    try {
+      const restaurantSearchResult = await docClient.query({
+        TableName: 'tiociufoodTable',
+        IndexName: 'pk',
+        KeyConditionExpression: '#pk = :pk',
+        ExpressionAttributeNames: {
+            '#pk': 'pk'
+        },
+        ExpressionAttributeValues: {
+            ':pk': `RESTAURANT`
+        }
+      }).promise()
+      return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        body: JSON.stringify({
+            message: 'success get restaurant',
+            restaurants: restaurantSearchResult
+        })
+      }
+    } catch(err) {
       return {
         statusCode: 500,
         headers: {
