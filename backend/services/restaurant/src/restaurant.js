f0a2063b91313575c17610d5d071b23592812663
patrick Patrick <cihuyman95@gmail.com>
2021-11-05 01:48:59 +0700

    add restaurant post

---
+++ backend/services/restaurant/src/restaurant.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const AWS = require('aws-sdk')
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+  region: 'us-east-2'
+})
+
+
+module.exports = {
+  addRestaurant: async (event) => {
+    // for dev
+    // const { name, address, description } = event
+
+    // for staging
+    const data = JSON.parse(event.body)
+    const { name, address, description } = data
+
+    try {
+      await docClient.put({
+        TableName: tiociufoodTable,
+        Item: {
+          pk: `RESTAURANT#${name}`,
+          sk: `RESTAURANT#${name}`,
+          restaurantName: name,
+          restaurantAddress: address,
+          restaurantDescription: description
+        }
+      }).promise()
+      return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        body: JSON.stringify({
+            message: 'success add restaurant'
+        })
+      }
+    } catch(err) {
+      return {
+        statusCode: 500,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        body: JSON.stringify({
+            message: 'something is wrong on the server',
+            error: err
+        })
+      }
+    }
+  }
+};
