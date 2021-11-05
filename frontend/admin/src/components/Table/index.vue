cf60a39af56923a291f295e5375698362fd6adf2
patrick Patrick <cihuyman95@gmail.com>
2021-11-06 05:06:44 +0700

    implement restaurant table

--- frontend/admin/src/components/Table/index.vue
+++ frontend/admin/src/components/Table/index.vue
@@ -11,9 +11,9 @@
         <tbody>
             <tr v-for="(content, index) in data" :key="index">
                 <td class="td"> {{ index + 1 }} </td>
-                <td class="td"> {{ content.restaurantName }} </td>
-                <td class="td"> {{ content.restaurantAddress }} </td>
-                <td class="td"> {{ content.restaurantDescription }} </td>
+                <td class="td" v-for="(key, keyIndex) in contentKeys" :key="keyIndex"> 
+                    {{ content[key] }} 
+                </td>
             </tr>
         </tbody>
     </table>
@@ -29,6 +29,14 @@ export default {
         data: {
             type: Array,
             required: true
+        }
+    },
+    computed: {
+        contentKeys() {
+            if (this.data.length === 0) {
+                return []
+            }
+            return Object.keys(this.data[0])
         }
     }
 }
