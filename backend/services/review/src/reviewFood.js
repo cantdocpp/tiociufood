9105a64aad3bcaddabab16fff42637fa1432cbeb
patrick Patrick <cihuyman95@gmail.com>
2021-11-30 05:41:29 +0700

    implement food review

---
+++ backend/services/review/src/reviewFood.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const AWS = require('aws-sdk')
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2'
+})
+
+module.exports = {
+    handler: async (event) => {
+        const data = JSON.parse(event.body)
+        const { userEmail, restaurantName, reviews } = data
+
+        try {
+            reviews.forEach(review => {
+                await docClient.put({
+                    TableName: 'mainTable',
+                    Item: {
+                        identifier: `FOOD#${review.foodName}`,
+                        sk: `REVIEW#${userEmail}`,
+                        restaurantName: restaurantName,
+                        foodRate: review.rate,
+                        reviewContent: review.review,
+                        reviewRate: rate,
+                    }
+                }).promise()
+
+                await docClient.put({
+                    TableName: 'mainTable',
+                    Item: {
+                        identifier: `USER#${userEmail}`,
+                        sk: `REVIEW#${review.foodName}`,
+                        restaurantName: restaurantName,
+                        foodName: review.foodName,
+                        foodRate: review.rate,
+                        reviewContent: review.review,
+                        reviewRate: rate,
+                    }
+                }).promise()
+            })
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'success add food review'
+                })
+            }
+        } catch(err) {
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify(err.message)
+            }
+        }
+    }
+}
