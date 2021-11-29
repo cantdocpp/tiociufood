d394411cf50d1c6c5f572a5e08c3d77e1b655bc6
patrick Patrick <cihuyman95@gmail.com>
2021-11-30 04:39:56 +0700

    change service name

---
+++ backend/services/review/src/reviewRestaurant.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const AWS = require('aws-sdk')
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2'
+})
+
+module.exports = {
+    handler: async (event) => {
+
+    }
+}
