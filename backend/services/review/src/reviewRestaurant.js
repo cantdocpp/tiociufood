826a82ac96e71e5b6f5d1707e1fe4c07956f3ff4
patrick Patrick <cihuyman95@gmail.com>
2021-11-30 05:14:25 +0700

    implement restaurant review

--- backend/services/review/src/reviewRestaurant.js
+++ backend/services/review/src/reviewRestaurant.js
@@ -8,6 +8,43 @@ const docClient = new AWS.DynamoDB.DocumentClient({
 
 module.exports = {
     handler: async (event) => {
+        const data = JSON.parse(event.body)
+        const { userEmail, restaurantName, rate, review } = data
 
+        try {
+            await docClient.put({
+                TableName: 'mainTable',
+                Item: {
+                    identifier: `RESTAURANT#${restaurantName}`,
+                    sk: `REVIEW#${restaurantName}`,
+                    restaurantName: restaurantName,
+                    userEmail: userEmail,
+                    reviewRate: rate,
+                    reviewContent: review
+                }
+            }).promise()
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'success add restaurant review'
+                })
+            }
+        } catch(err) {
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify(err.message)
+            }
+        }
     }
 }
