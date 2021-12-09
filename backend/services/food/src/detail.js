724c0cbaa8270568429b3487b50721a3913c991d
patrick Patrick <cihuyman95@gmail.com>
2021-12-10 02:51:52 +0700

    finish get food detail endpoint

---
+++ backend/services/food/src/detail.js
@@ -0,0 +1,70 @@
+const AWS = require('aws-sdk')
+
+AWS.config.update({region: 'us-east-2'});
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2'
+})
+
+module.exports = {
+    handler: async (event) => {
+        const data = JSON.parse(event.body)
+        const { foodName } = data
+
+        try {
+            const foodDetail = await docClient.query({
+                TableName: 'mainTable',
+                KeyConditionExpression: '#identifier = :identifier AND #sk = :sk',
+                ExpressionAttributeNames: {
+                    '#identifier': 'identifier',
+                    '#sk': 'sk'
+                },
+                ExpressionAttributeValues: {
+                    ':identifier': `FOOD#${foodName}`,
+                    ':sk': `FOOD#${foodName}`
+                }
+            }).promise()
+
+            const restaurantList = await docClient.scan({
+                TableName: 'mainTable',
+                FilterExpression: 'begins_with(#identifier, :identifier) AND contains(#sk, :sk)',
+                ProjectionExpression: 'restaurantName, restaurantAddress, restaurantImage',
+                ExpressionAttributeNames: {
+                    '#identifier': 'identifier',
+                    '#sk': 'sk'
+                },
+                ExpressionAttributeValues: {
+                    ':identifier': 'RESTAURANT',
+                    ':sk': 'RESTAURANT'
+                }
+              }).promise()
+
+            return {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'success get food detail',
+                    foodDetailData: foodDetail,
+                    restaurantData: restaurantList
+                })
+            }
+        } catch(err) {
+            return {
+                statusCode: 500,
+                headers: {
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+                },
+                body: JSON.stringify({
+                    message: 'something is wrong on the server',
+                    error: JSON.stringify(err.message)
+                })
+            }
+        }
+    }
+}
