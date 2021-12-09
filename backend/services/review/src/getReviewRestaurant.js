55710fbb2b769d94417b8609048840a08f9d76ca
patrick Patrick <cihuyman95@gmail.com>
2021-12-09 23:18:58 +0700

    add get restaurant review endpoint

---
+++ backend/services/review/src/getReviewRestaurant.js
@@ -0,0 +1,54 @@
+const AWS = require('aws-sdk')
+
+AWS.config.update({region: 'us-east-2'});
+
+const docClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2'
+})
+
+module.exports = {
+    handler: async (event) => {
+        const data = JSON.parse(event.body)
+        const { restaurantName } = data
+        try {
+            const restaurantReview = await docClient.query({
+              TableName: 'mainTable',
+              FilterExpression: '#identifier = :identifier AND contains(#sk, :sk)',
+              ExpressionAttributeNames: {
+                  '#identifier': 'identifier',
+                  '#sk': 'sk'
+              },
+              ExpressionAttributeValues: {
+                  ':identifier': `RESTAURANT#${restaurantName}`,
+                  ':sk': 'REVIEW'
+              }
+            }).promise()
+            
+            return {
+              statusCode: 200,
+              headers: {
+                  "Access-Control-Allow-Headers": "Content-Type",
+                  "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+              },
+              body: JSON.stringify({
+                  message: 'success get restaurant review',
+                  restaurantData: restaurantReview.Items
+              })
+            }
+          } catch(err) {
+            return {
+              statusCode: 500,
+              headers: {
+                  "Access-Control-Allow-Headers": "Content-Type",
+                  "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+              },
+              body: JSON.stringify({
+                  message: 'something is wrong on the server',
+                  error: JSON.stringify(err.message)
+              })
+            }
+        }
+    }
+}
